d4b5d9297b41926632f79a12b50e2d3f7de02908
1-chris <60541438+1-chris@users.noreply.github.com>
2023-02-25 23:14:24 +0000

    Update run.ps1

--- run.ps1
+++ run.ps1
@@ -1,10 +1,5 @@
 param($Timer)
 
-# Don't leak your bot token
-if ($null -eq $BotToken) {
-    $BotToken = $env:BotToken
-}
-
 while ($true) {
     # Keep run.ps1 running
     Start-Sleep -Seconds 1
@@ -13,7 +8,7 @@ while ($true) {
         Write-Host "Invoking Invoke-DiscordBot.ps1 at $(Get-Date)"
 
         # Invoke the Discord bot script
-        & $PSScriptRoot/Invoke-DiscordBot.ps1 -BotToken $BotToken
+        & $PSScriptRoot/Invoke-DiscordBot.ps1
     } catch {
         Write-Host "Error on Invoke-DiscordBot.ps1 invocation at $(Get-Date). Error: $_ - $($_.Exception.Message)"
         Write-Host "Restarting Invoke-DiscordBot.ps1 at $(Get-Date)"
